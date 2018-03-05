a96c440339033096b5bb4d312633d92fc078299e
Said <saidsl@users.noreply.github.com>
2018-03-05 21:48:16 +0800

    Rename DocType, Fields and add Fields
    Renamed DocType from Event to Activity
    Added Activity End Time
    Made Report date, Start TIme & End Time Mandatory

---
+++ aggregate_production/aggregate_production/doctype/production_activity/production_activity.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2018, ERP Admin and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Production Activity', {
+	refresh: function(frm) {
+
+	}
+});
